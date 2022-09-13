77be351ca7b2c8a3c4add9c54d61e47744993327
luizgnclvs <luizgnclvss@gmail.com>
2022-09-13 03:52:23 -0300

    formatTable resolved

--- password-game/src/components/formatTable.jsx
+++ password-game/src/components/formatTable.jsx
@@ -3,13 +3,16 @@ let regexes = [/[aáàãâ]/i, /[eé]/i, /[ií]/i, /[oóõoô]/i, /[uú]/i, /[c�
 const matchRegex = (char) => {
     let match = null;
     let regex = -1;
+
     for (let i = 0; i < regexes.length; i++) {
         match = char.match(regexes[i]);
+
         if (match !== null) {
             regex = i;
             break;
         }
     }
+
     return {match, regex};
 };
 
@@ -19,6 +22,7 @@ export const formatTable = () => {
 
     for (let row of rows) {
         let cellElements = row.children;
+
         let cells = [];
         let letters = [];
 
@@ -27,46 +31,40 @@ export const formatTable = () => {
             letters.push({value: inputs[i].id, matched: false,  position: i});
         }
 
-        console.log(cells)
-        console.log(letters)
+        for (let letter of letters) {
+            let cell = cells.find(object => object.position === letter.position);
+
+            let letterMatch = matchRegex(letter.value);
+            let cellMatch = matchRegex(cell.value);
+
+            if (letterMatch.regex === cellMatch.regex) {
+                if (letterMatch.match && cellMatch.match) {
+                    cell.cell.classList.add("bull");
+                    letter.matched = true;
+                } else {
+                    if (letter.value === cell.value) {
+                        cell.cell.classList.add("bull");
+                        letter.matched = true;
+                    }
+                }
+            }
+        }
 
         for (let cell of cells) {
-            console.log(cell)
             let cellMatch = matchRegex(cell.value);
-            console.log(cellMatch)
-            
+
             for (let letter of letters) {
-                console.log(letter)
-                console.log(letter.matched)
                 if (!letter.matched) {
                     let letterMatch = matchRegex(letter.value);
-                    console.log(letterMatch)
-                    console.log(letterMatch.regex === cellMatch.regex)
-                    if (letterMatch.regex === cellMatch.regex) {
-                        console.log(letterMatch.match)
-                        console.log(cellMatch.match)
-                        if (letterMatch.match && cellMatch.match) {
-                            console.log(letter.position)
-                            console.log(cell.position)
-                            console.log(letter.position === cell.position)
-                            if (letter.position === cell.position) {
-                                cell.cell.classList.add("bull");
-                            } else {
-                                cell.cell.classList.add("cow");
-                            }
 
+                    if (letterMatch.regex === cellMatch.regex) {
+                        if (letterMatch.match && cellMatch.match) {
+                            cell.cell.classList.add("cow");
                             letter.matched = true;
                             break;
                         } else {
-                            console.log(letter.value === cell.value)
                             if (letter.value === cell.value) {
-                                console.log(letter.position === cell.position)
-                                if (letter.position === cell.position) {
-                                    cell.cell.classList.add("bull");
-                                } else {
-                                    cell.cell.classList.add("cow");
-                                }
-
+                                cell.cell.classList.add("cow");
                                 letter.matched = true;
                                 break;
                             } else {
@@ -78,8 +76,6 @@ export const formatTable = () => {
                     }
                 }
             }
-            console.log(letters)
         }
-        console.log(cells)
     }
 };
