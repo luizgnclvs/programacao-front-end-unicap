5536b619587bfe7feed1df551207d3cbfdf02f87
luizgnclvs <luizgnclvss@gmail.com>
2022-11-08 14:58:20 -0300

    baseFriendship adicionado

--- React UI Framework/react-ui-framework/src/models/PokemonModel.js
+++ React UI Framework/react-ui-framework/src/models/PokemonModel.js
@@ -1,15 +1,16 @@
 export default class Pokemon {
-    constructor(number, name, image, height, weight, types, entry, abilities, genderRatio, catchRate, baseExp) {
+    constructor(number, name, image, height, weight, types, entry, abilities, genderRatio, catchRate, baseExperience, baseFriendship) {
         this.number = ;number
         this.name = name;
         this.image = image;
         this.height = height;
-        this.weight =weight;
+        this.weight = weight;
         this.types = types;
         this.entry = entry;
         this.abilities = abilities;
         this.genderRatio = genderRatio;
         this.catchRate = catchRate;
-        this.baseExp = baseExp;
+        this.baseExperience = baseExperience;
+        this.baseFriendship = baseFriendship;
     }
 }
